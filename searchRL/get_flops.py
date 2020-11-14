94bfe71203c0b9635013c0622d1c4377b22f4e09
thearkamitra <39183864+thearkamitra@users.noreply.github.com>
2020-11-14 21:04:18 +0530

    Check once

---
+++ searchRL/get_flops.py
@@ -0,0 +1,13 @@
+from thop import profile
+import torch
+def find_flops(model, input = -1):
+    '''
+        The input needs to be a PyTorch model and it will return the number
+        of flops and parameters of the model.
+        The dataset is considered to be CIFAR10 and the batch size is 1.
+    '''
+    if type(input) == int:
+        input = torch.randn(1, 3, 32, 32)
+
+    macs, params = profile(model, inputs=(input, ))
+    print(f"Flops of the model: {macs}; Parameters of the model:{params}")
